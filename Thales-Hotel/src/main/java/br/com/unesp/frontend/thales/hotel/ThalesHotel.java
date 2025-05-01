7ae12cb62201d44afc0c82444c637a6ae7698bcd
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2025-05-01 19:32:56 -0300

    colocando tema e primeiro frame

--- Thales-Hotel/src/main/java/br/com/unesp/frontend/thales/hotel/ThalesHotel.java
+++ Thales-Hotel/src/main/java/br/com/unesp/frontend/thales/hotel/ThalesHotel.java
@@ -4,6 +4,10 @@
 
 package br.com.unesp.frontend.thales.hotel;
 
+import br.com.unesp.frontend.thales.hotel.forms.init.Inicio;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import javax.swing.UIManager;
+
 /**
  *
  * @author Eduks
@@ -11,6 +15,13 @@ package br.com.unesp.frontend.thales.hotel;
 public class ThalesHotel {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try{
+            UIManager.setLookAndFeel(new FlatMacLightLaf());
+            Inicio inicio = new Inicio();
+            inicio.setVisible(true);
+            inicio.setResizable(false);
+        }catch(Exception e){
+            System.err.println("Ocorreu um erro ao tentar inicializar o tema");
+        }
     }
 }
