f0464b9a407410fbf0bdb9b5ca0a1f6786ad264a
Eduardo Miguel dos Reis Teixeira <eduksoficial@gmail.com>
2025-05-01 18:54:35 -0300

    projeto

---
+++ Thales-Hotel/src/main/java/br/com/unesp/frontend/thales/hotel/ThalesHotel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.unesp.frontend.thales.hotel;
+
+/**
+ *
+ * @author Eduks
+ */
+public class ThalesHotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
